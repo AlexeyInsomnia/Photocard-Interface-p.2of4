097fe3c15a8ae9c71849adf96f10bb060577e153
AlexeyUr <alisvcom@gmail.com>
2016-02-29 01:27:13 +0300

    1

---
+++ iHelper/iHelper/APQuestionViewController.h
@@ -0,0 +1,15 @@
+//
+//  APQuestionViewController.h
+//  iHelper
+//
+//  Created by Alex on 18.02.16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APQuestionViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
